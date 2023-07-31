87b58e4ee634b33f9c3a05160f5bf29b94bba51e
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-07-31 15:18:46 +0800

    Create VerifyAPIKey.php

---
+++ app/Http/Middleware/VerifyAPIKey.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class VerifyAPIKey
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->header('x-api-key')) {
+            $apiKey = $request->header('x-api-key');
+
+            if ($apiKey !== env('API_KEY')) {
+                return response()->json(['message' => 'Invalid Access Key'], 400);
+            } else {
+                return $next($request);
+            }
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
