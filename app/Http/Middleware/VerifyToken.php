d4912a3cca50f58f5f573fa4ec1c4e8b7cf38a5a
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-07-31 15:18:53 +0800

    Create VerifyToken.php

---
+++ app/Http/Middleware/VerifyToken.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use ReallySimpleJWT\Token;
+use App\Models\User;
+
+class VerifyToken
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->header('Authorization')) {
+            $authorizationHeader = $request->header('Authorization');
+            $token = str_replace('Bearer ', '', $authorizationHeader);
+
+            $secret = 'Hello&MikeFooBar123';
+
+            if (!Token::validate($token, $secret)) {
+                return response()->json([
+                    'message' => 'Invalid Authorization Token',
+                ], 400);
+            } else {
+
+                // dd(Token::getPayload($token, $secret));
+                // dd(Token::getHeader($token, $secret));
+
+                $user = User::find(Token::getPayload($token, $secret)['uid']);
+
+                if (!$user) {
+                    return response()->json([
+                        'message' => 'User not found',
+                    ], 404);
+                }
+
+                $request->merge([
+                    'user_data' => [
+                        'name' => $user->name,
+                        'email' => $user->email,
+                    ]
+                ]);
+
+                return $next($request);
+            }
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
