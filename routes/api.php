8f25cd80c5c1aa27eac5597a5a151bb2afb913c8
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-08-04 16:19:58 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// User signing in
+/**
+ * AUTH ROUTES
+ */
 Route::post('/account/login', 'AuthController@login')->middleware(['validate-api-key'])->name('login');
-
-// User signing up
 Route::post('/account/register', 'AuthController@register')->middleware(['validate-api-key'])->name('register');
 
-// PROTECTED: User Profile
+/**
+ * PROTECTED: User Profile
+ */
 Route::get('/account/profile', 'PrivateRoutes@profile')
     ->middleware(['validate-access-token', 'validate-api-key'])
     ->name('profile');
+
+
+/**
+ * USER ROUTES
+ */
+Route::post('/users', 'UserController@store')
+    ->middleware(['validate-api-key'])
+    ->name('user-create');
