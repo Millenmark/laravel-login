7f1a9847980ff62adb8b37e9e74ed4c250bbd47f
Millen Mark Aquino <millenmark.dev@gmail.com>
2023-08-06 14:39:08 +0800

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,6 +34,8 @@ class User extends Authenticatable
         'role',
         'email_verified_at',
         'password',
+        'isPublic',
+        'about'
     ];
 
     /**
@@ -54,4 +56,30 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected function toCamelCase($value)
+    {
+        $segments = explode('_', $value);
+
+        array_walk($segments, function (&$item, $key) {
+            if ($key > 0) {
+                $item = ucfirst($item);
+            }
+        });
+
+        return implode('', $segments);
+    }
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+
+        $newArray = [];
+        foreach ($array as $key => $value) {
+            $newKey = $this->toCamelCase($key);
+            $newArray[$newKey] = $value;
+        }
+
+        return $newArray;
+    }
 }
